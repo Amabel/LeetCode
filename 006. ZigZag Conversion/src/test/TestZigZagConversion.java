79808f7e3784349aac7028bff1cd15753a1da931
Weibin Luo <luoweibinb@gmail.com>
2017-06-29 19:40:46 +0900

    testcases

---
+++ 006. ZigZag Conversion/src/test/TestZigZagConversion.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import solution.ZigZagConversion;
+
+public class TestZigZagConversion {
+	
+	ZigZagConversion zzc;
+	
+	
+
+	@Before
+	public void setUp() throws Exception {
+		zzc = new ZigZagConversion();
+	}
+
+	@Test
+	public void testSolution_2() {
+		fail("Not yet implemented");
+	}
+
+}
