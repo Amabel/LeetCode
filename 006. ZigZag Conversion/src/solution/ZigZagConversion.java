4ac00620776d508057d9cbf26d628152d78b4604
Weibin Luo <luoweibinb@gmail.com>
2017-07-02 01:07:33 +0900

    replace tabs

--- 006. ZigZag Conversion/src/solution/ZigZagConversion.java
+++ 006. ZigZag Conversion/src/solution/ZigZagConversion.java
@@ -6,68 +6,68 @@ package solution;
  */
 public class ZigZagConversion {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println(new ZigZagConversion().solution_2("PAYPALISHIRING", 3));
-	}
+        System.out.println(new ZigZagConversion().solution_2("PAYPALISHIRING", 3));
+    }
 
-	public String solution_1(String s, int numRows) {
-		if (null == s || numRows < 1) {
-			throw new IllegalArgumentException("s is null or numRows < 1");
-		}
-		if ("" == s || numRows < 2) {
-			return s;
-		}
+    public String solution_1(String s, int numRows) {
+        if (null == s || numRows < 1) {
+            throw new IllegalArgumentException("s is null or numRows < 1");
+        }
+        if ("" == s || numRows < 2) {
+            return s;
+        }
 
-		String[] rets = new String[numRows];
+        String[] rets = new String[numRows];
 
-		// init
-		for (int i=0; i<numRows; i++) {
-			rets[i] = "";
-		}
+        // init
+        for (int i=0; i<numRows; i++) {
+            rets[i] = "";
+        }
 
-		int i = 0;
-		int len = s.length();
-		while (i < len) {
-			// idx for row index
-			// down
-			for (int idx=0; idx<numRows && i<len; idx++) {
-				rets[idx] += s.charAt(i);
-				i = i + 1;
-			}
-			// up
-			for (int idx=numRows-2; idx>0 && i<len; idx--) {
-				rets[idx] += s.charAt(i);
-				i = i + 1;
-			}
-		}
-		return String.join("", rets);
-	}
+        int i = 0;
+        int len = s.length();
+        while (i < len) {
+            // idx for row index
+            // down
+            for (int idx=0; idx<numRows && i<len; idx++) {
+                rets[idx] += s.charAt(i);
+                i = i + 1;
+            }
+            // up
+            for (int idx=numRows-2; idx>0 && i<len; idx--) {
+                rets[idx] += s.charAt(i);
+                i = i + 1;
+            }
+        }
+        return String.join("", rets);
+    }
 
-	public String solution_2(String s, int numRows) {
-		if (null == s || numRows < 1) {
-			throw new IllegalArgumentException("s is null or numRows < 1");
-		}
-		if ("" == s || numRows < 2) {
-			return s;
-		}
-		char[] ch = s.toCharArray();
-		String[] rets = new String[numRows];
-		// init
-		for (int i=0; i<numRows; i++) {
-			rets[i] = "";
-		}
+    public String solution_2(String s, int numRows) {
+        if (null == s || numRows < 1) {
+            throw new IllegalArgumentException("s is null or numRows < 1");
+        }
+        if ("" == s || numRows < 2) {
+            return s;
+        }
+        char[] ch = s.toCharArray();
+        String[] rets = new String[numRows];
+        // init
+        for (int i=0; i<numRows; i++) {
+            rets[i] = "";
+        }
 
-		for (int i=0; i<s.length(); i++) {
-			int m = i % (numRows - 1);
-			int n = i / (numRows - 1);
-			if (n % 2 == 0) {
-				rets[m] += ch[i];
-			} else {
-				rets[numRows - m - 1] += ch[i];
-			}
-		}
-		return String.join("", rets);
-	}
+        for (int i=0; i<s.length(); i++) {
+            int m = i % (numRows - 1);
+            int n = i / (numRows - 1);
+            if (n % 2 == 0) {
+                rets[m] += ch[i];
+            } else {
+                rets[numRows - m - 1] += ch[i];
+            }
+        }
+        return String.join("", rets);
+    }
 
 }
