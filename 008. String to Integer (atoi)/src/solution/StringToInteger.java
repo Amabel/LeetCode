441a3427785f5ec0bb54ef4755a29a9c267c0b36
Weibin Luo <luoweibinb@gmail.com>
2017-07-04 17:55:35 +0900

    solution

--- 008. String to Integer (atoi)/src/solution/StringToInteger.java
+++ 008. String to Integer (atoi)/src/solution/StringToInteger.java
@@ -5,8 +5,8 @@ public class StringToInteger {
     // 考虑非数字的情况（报错）
     // 空指针
     public static void main(String[] args) {
-        String str = "123";
-        int res = new StringToInteger.solution(str);
+        String str = "3147483644";
+        int res = new StringToInteger().solution(str);
         System.out.println(res);
     }
 
@@ -14,14 +14,25 @@ public class StringToInteger {
         if (null == str) {
             throw new IllegalArgumentException("str is null!");
         }
+        if (str.length() > 10) {
+            throw new IllegalArgumentException("The number is too big!");
+        }
+        if (0 == str.length()) {
+            throw new IllegalArgumentException("str must only contain numbers!");
+        }
 
         int ret = 0;
         int len = str.length();
 
-        for (int i=0; i<len< i++) {
+        for (int i=0; i<len; i++) {
             char c = str.charAt(i);
-            int digit = c - "0";
+            int digit = c - '0';
             if (0<= digit && digit <= 9) {
+                if (9 == i) {
+                    if (ret > 214748364 || (ret <= 214748364 && digit >= 8)) {
+                        throw new IllegalArgumentException("The number is too big!");
+                    }
+                }
                 ret = ret * 10 + digit;
             } else {
                 throw new IllegalArgumentException("str must only contain numbers!");
