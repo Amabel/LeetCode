23ab2e7f4bc7028ecb2091ab8da892d5e8c5588f
Weibin Luo <luoweibinb@gmail.com>
2017-07-05 17:21:28 +0900

    delete unused method

--- 008. String to Integer (atoi)/src/solution/StringToInteger.java
+++ 008. String to Integer (atoi)/src/solution/StringToInteger.java
@@ -1,44 +1,11 @@
 package solution;
 
 public class StringToInteger {
-    // 考虑长度大于 32-bit 的情况
-    // 考虑非数字的情况（报错）
-    // 空指针
+
     public static void main(String[] args) {
         String str = "-2147483648";
-        int res = new StringToInteger().solution(str);
+        int res = new StringToInteger().solution_1(str);
         System.out.println(res);
-    }
-
-    public int solution_(String str) {
-        if (null == str) {
-            throw new IllegalArgumentException("str is null!");
-        }
-        if (str.length() > 10) {
-            throw new IllegalArgumentException("The number is too big!");
-        }
-        if (0 == str.length()) {
-            throw new IllegalArgumentException("str must only contain numbers!");
-        }
-
-        int ret = 0;
-        int len = str.length();
-
-        for (int i=0; i<len; i++) {
-            char c = str.charAt(i);
-            int digit = c - '0';
-            if (0<= digit && digit <= 9) {
-                if (9 == i) {
-                    if (ret > 214748364 || (ret <= 214748364 && digit >= 8)) {
-                        throw new IllegalArgumentException("The number is too big!");
-                    }
-                }
-                ret = ret * 10 + digit;
-            } else {
-                throw new IllegalArgumentException("str must only contain numbers!");
-            }
-        }
-        return ret;
     }
 
     public int solution_1(String str) {
